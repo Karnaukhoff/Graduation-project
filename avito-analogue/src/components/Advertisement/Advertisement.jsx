b1a2807128c4e51df01213b3a4e02feda47f4631
karnaukhoff <n4echduf@gmail.com>
2024-02-16 13:40:50 +0300

    Сделан формат цены

--- avito-analogue/src/components/Advertisement/Advertisement.jsx
+++ avito-analogue/src/components/Advertisement/Advertisement.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import * as S from "./Advertisement-styles";
 
-function Advertisement({images, title, price, city, released}) {
+function Advertisement({ images, title, price, city, released }) {
   //1. показать изображение
-  //2. формат цены
+  function priceFormat(price) {
+    let result = "";
+    let priceLengthDevide = Math.ceil(price.toString().length / 3) + 1;
+    while (price > 0) {
+      if (priceLengthDevide > 2){
+        result += Math.floor(price / Math.pow(10, priceLengthDevide)).toString() + " ";
+        price -= Math.floor(price / Math.pow(10, priceLengthDevide))* Math.pow(10, priceLengthDevide);
+        priceLengthDevide--;
+      }
+
+      if (priceLengthDevide <= 2 && price > 0) {
+        result += price.toString();
+        price = 0;
+      }
+      else if (priceLengthDevide <= 2 && price === 0) result += "000"
+    }
+    return result;
+  }
+  console.log();
   //3. формат даты
   //4. переключение фотографий
   return (
@@ -16,11 +34,9 @@ function Advertisement({images, title, price, city, released}) {
         </S.AdvImg>
         <S.AdvContent>
           <S.AdvContentLink>
-            <S.AdvContentTitle>
-              {title}
-            </S.AdvContentTitle>
+            <S.AdvContentTitle>{title}</S.AdvContentTitle>
           </S.AdvContentLink>
-          <S.AdvContentPrice>{price} ₽</S.AdvContentPrice>
+          <S.AdvContentPrice>{priceFormat(price)} ₽</S.AdvContentPrice>
           <S.AdvContentPlace>{city}</S.AdvContentPlace>
           <S.AdvContentDate>{released}</S.AdvContentDate>
         </S.AdvContent>
