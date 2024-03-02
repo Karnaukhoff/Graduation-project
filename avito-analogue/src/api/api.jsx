796336639eff17f4d2ed75beffc7e016fe70e670
karnaukhoff <n4echduf@gmail.com>
2024-03-02 18:18:24 +0300

    Проблема 3/6 решена: регистрация

--- avito-analogue/src/api/api.jsx
+++ avito-analogue/src/api/api.jsx
@@ -18,6 +18,7 @@ export async function signUp({ email, password, name, surname, city }) {
       surname: surname,
       city: city,
       role: "user",
+      phone: ""
     }),
     headers: {
       "content-type": "application/json",
